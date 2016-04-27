49af2a76dd75d5b05ce8a50b09b4bea0332f38be
Rocky1987 <decievemyself@gmail.com>
2016-04-27 15:53:48 +0800

    Add a.txt

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require('express');//載入express模組
 var app = express();//初始化
-app.get('/', function(req, res){
-	res.send('安安!!express~  this is a index');
-})
+app.use(express.static('public'));
 app.listen(1337,function(){//設定ROUTER首頁的處理函式
 	console.log('read on port 1337');
 })
